66722218e67e12287105955f71d18e1e3e276198
Sam <samuel.waters@gmail.com>
2022-11-24 15:58:49 -0500

    chore:working on callmapbox function calling

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const apiKeyWeather = 'db56fd11ee89595a2da699cbbda005b4';
 const city = 'New York';
 const weatherUrl = `http://api.weatherstack.com/current?access_key=${apiKeyWeather}&query=${city}`;
 const apiKeyMap = 'pk.eyJ1IjoiZGFya2NvaGliYSIsImEiOiJjbDNkdzd5YmQwMDJzM2NuOGRjMzMybzlmIn0.zTD1-NwCvNlExYYFWxvzmQ'
-const mapBoxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?proximity=ip&access_token=${apiKeyMap}`
+
 
 function callWeather(){
     request({ url: weatherUrl, json: true}, (error, response) => {
@@ -18,16 +18,24 @@ function callWeather(){
         });
 }
 
-function callMapBox(){
+function callMapBox(address, callback) {
+    const mapBoxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/` + address + `.json?access_token=${apiKeyMap}`;
+
         request({ url: mapBoxUrl, json: true}, (error, response) => {
             if (error) {
-                console.log("Unable to connect to location services!");
+                callback("Unable to connect to location services!", undefined);
             } else if (response.body.features[0] === undefined) {
-                console.log('Unable to find location. Try another search.');
+                callback('Unable to find location. Try another search.', undefined);
             } else {
                 const latitude = response.body.features[0].center[1];
                 const longitude = response.body.features[0].center[0];
-                console.log(`latitude is ${latitude}, longitude is ${longitude}`);
+                callback(undefined, `latitude is ${latitude}, longitude is ${longitude}`);
             }
         });
 }
+
+callMapBox('Boston', (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data);
+    }
+);
--- math.js
+++ math.js
@@ -19,8 +19,26 @@ function countUp(number) {
     return "number counted up";
 }
 
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+
+function add(num1, num2, callback) {
+    setTimeout(() => {
+        const sum = num1 + num2;
+        callback(sum);
+    }, 2000);
+}
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
+})
+
 module.exports = {
     countDown: countDown,
     countUp: countUp
-
 };
