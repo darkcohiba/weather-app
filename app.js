37d1d1f460bfb4ca9ff83a11d69238bfb3d4d836
Sam <samuel.waters@gmail.com>
2022-11-24 20:54:55 -0500

    chore:finished adding function for call weather

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const request = require('postman-request');
 
 const apiKeyWeather = 'db56fd11ee89595a2da699cbbda005b4';
-const city = 'New York';
-const weatherUrl = `http://api.weatherstack.com/current?access_key=${apiKeyWeather}&query=${city}`;
 const apiKeyMap = 'pk.eyJ1IjoiZGFya2NvaGliYSIsImEiOiJjbDNkdzd5YmQwMDJzM2NuOGRjMzMybzlmIn0.zTD1-NwCvNlExYYFWxvzmQ'
 
 
-function callWeather(){
+function callWeather(city, callback){
+    const weatherUrl = `http://api.weatherstack.com/current?access_key=${apiKeyWeather}&query=${city}`;
     request({ url: weatherUrl, json: true}, (error, response) => {
         if (error) {
-            console.log('Unable to connect to weather service!');
+            callback('Unable to connect to weather service!', undefined);
         } else if (response.body.error) {
-            console.log('Unable to find location');
+            callback('Unable to find location', undefined);
         }else {
-            console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.');
+            callback(undefined, response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.');
         }
         });
 }
+
+
+callWeather('Boston', (error, data) => {
+    if (error) {
+        console.log('Error', error);
+    } else {
+        console.log('Data', data);
+    }
+}
+);
 
 function callMapBox(address, callback) {
     const mapBoxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/` + address + `.json?access_token=${apiKeyMap}`;
@@ -34,8 +43,11 @@ function callMapBox(address, callback) {
         });
 }
 
-callMapBox('Boston', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-    }
-);
+// callMapBox('Boston', (error, data) => {
+//         if (error) {
+//             console.log('Error', error);
+//         } else {
+//             console.log('Data', data);
+//         }
+//     }
+// );
